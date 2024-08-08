2134854f3a1b08523d18d2ed3a6c941f9d3e37ad
Quocviet Le <quocvietlet@gmail.com>
2024-08-08 00:41:28 -0500

    final commit

--- word_freq.py
+++ word_freq.py
@@ -1,14 +1,21 @@
-""" A program to count frequencies of all words in a file. """
+import os
+
+# script to count the frequency of words in a text file
 
 
+# function to sort by frequency
 def byFreq(pair):
-    """A function that returns the second item of a pair."""
     return pair[1]
 
 
+# main function
 def main():
-    """The main function."""
+
+    folder_path = (
+        "C:/Users/Q/Documents/Important Stuff/Python Work/Projects/word_counter"
+    )
     filename = input("Give the name of the file: ").strip()
+    file_path = os.path.join(folder_path, filename)
     try:
         with open(filename, "r") as infile:
             text = infile.read()  # read the entire file in a string
@@ -18,6 +25,8 @@ def main():
     except OSError as e:
         print(f"Error reading file '{filename}': {e}")
         return
+
+    # remove punctuation characters from the text
     for ch in "!'\"#$%&()*+,-./:;<=>?@[\\]^_`{|}~":
         # loop through all these characters, replacing each by space
         # Note: charcters " and ' are included with a \ before them
@@ -35,8 +44,8 @@ def main():
     uniqueWords.sort(key=byFreq, reverse=True)
 
     # print the words and their frequencies
-    for w in uniqueWords:
-        print(w, counts[w])
+    for w, freq in uniqueWords:
+        print(w, freq)
 
 
 main()
