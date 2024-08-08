0bde935d358c1d36b1a787ec41de63bad847e73d
Quocviet Le <quocvietlet@gmail.com>
2024-08-07 23:54:48 -0500

    implemented use of byFreq

--- word_freq.py
+++ word_freq.py
@@ -1,38 +1,36 @@
 """ A program to count frequencies of all words in a file. """
 
+
 def byFreq(pair):
-    """ A function that returns the second item of a pair. """
+    """A function that returns the second item of a pair."""
     return pair[1]
 
-def main() :
-    """ The main function. """
+
+def main():
+    """The main function."""
     filename = input("Give the name of the file: ")
     infile = open(filename, "r")
-    text = infile.read() # read the entire file in a string
-    text = text.lower() # lowercase everything
-    for ch in "!\'\"#$%&()*+,-./:;<=>?@[\\]^_`{|}~":
+    text = infile.read()  # read the entire file in a string
+    text = text.lower()  # lowercase everything
+    for ch in "!'\"#$%&()*+,-./:;<=>?@[\\]^_`{|}~":
         # loop through all these characters, replacing each by space
         # Note: charcters " and ' are included with a \ before them
         text = text.replace(ch, " ")
 
-    words = text.split() # get words (tokens separated by whitespaces)
-    counts = {} # dictionary for frequency counts
-    for w in words :
-        counts[w] = counts.get(w,0) + 1  # increment counts
+    words = text.split()  # get words (tokens separated by whitespaces)
+    counts = {}  # dictionary for frequency counts
+    for w in words:
+        counts[w] = counts.get(w, 0) + 1  # increment counts
 
     # get list of words that appear in document
-    uniqueWords = list(counts.keys())
+    uniqueWords = list(counts.items())
 
     # put list of words in alphabetical order
-    uniqueWords.sort()
+    uniqueWords.sort(key=byFreq, reverse=True)
 
     # print words and associated counts
     for w in uniqueWords:
         print(w, counts[w])
 
+
 main()
-
-
-
-
-    
